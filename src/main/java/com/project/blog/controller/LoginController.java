cac03db91b170f062810f56eb5cd3d23a021f41e
NIRMAL KUMAR <nirmal.kumar.17.1@mountblue.tech>
2021-10-08 23:06:02 +0530

    Create method for user log in.

---
+++ src/main/java/com/project/blog/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.project.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/login")
+    public String loginPage(){
+        return "/login";
+    }
+    @RequestMapping("/logoutSuccess")
+    public String logout(){
+        return "/logoutSuccess";
+    }
+}
