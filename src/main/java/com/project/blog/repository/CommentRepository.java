46fae01b298110286965f3e41b85d761ad2e2ad1
NIRMAL KUMAR <nirmal.kumar.17.1@mountblue.tech>
2021-10-08 23:18:42 +0530

    make jpa queries for comment

---
+++ src/main/java/com/project/blog/repository/CommentRepository.java
@@ -0,0 +1,17 @@
+package com.project.blog.repository;
+
+import com.project.blog.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNullApi;
+import org.springframework.stereotype.Repository;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByPostId(Long PostId);
+
+    Optional<Comment> findById(Long Id);
+}
