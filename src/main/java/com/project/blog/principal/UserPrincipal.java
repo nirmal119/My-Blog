c575c9c8a4b44e6720e7aaf90c743b1eedaa1570
NIRMAL KUMAR <nirmal.kumar.17.1@mountblue.tech>
2021-10-11 02:57:46 +0530

    Refactor code and change java version to 8

--- src/main/java/com/project/blog/principal/UserPrincipal.java
+++ src/main/java/com/project/blog/principal/UserPrincipal.java
@@ -4,11 +4,13 @@ import com.project.blog.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class UserPrincipal implements UserDetails {
-    private  User user;
+    private final User user;
 
     public UserPrincipal(User user) {
         super();
@@ -17,8 +19,10 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
         SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(user.getRole());
-        return List.of(simpleGrantedAuthority);
+        authorities.add(simpleGrantedAuthority);
+        return authorities;
     }
 
     @Override
